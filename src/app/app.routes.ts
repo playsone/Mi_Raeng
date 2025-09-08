ea0a1b027917d3757665d9a08a2d433bd90b7d77
playsone <oammanat9091@gmail.com>
2025-09-08 22:12:06 +0700

    11

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Welcome} from './pages/welcome/welcome';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Home} from './pages/home/home';
+import { Exercise } from './pages/exercise/exercise';
+import { Ranking } from './pages/ranking/ranking';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: Welcome },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'home', component: Home },
+  { path: 'exercise', component: Exercise },
+  { path: 'ranking', component: Ranking }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/welcome/welcome.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './welcome.html',
+  styleUrl: './welcome.scss'
+})
+export class Welcome {
+
+}

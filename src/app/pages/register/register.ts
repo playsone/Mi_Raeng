2c26cd0041a22f9e9190fb5392266ef4bfcc1436
Poder2004 <66011212129@msu.ac.th>
2025-09-10 15:43:35 +0700

    แก้

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './register.scss'
 })
 export class Register {
+   // ตัวแปรสำหรับช่อง "รหัสผ่าน"
+  passwordFieldType: string = 'password';
+  passwordIcon: string = 'visibility';
+
+  // ตัวแปรสำหรับช่อง "ยืนยันรหัสผ่าน"
+  confirmPasswordFieldType: string = 'password';
+  confirmPasswordIcon: string = 'visibility';
+
+  constructor() { }
+
+
 
 }
